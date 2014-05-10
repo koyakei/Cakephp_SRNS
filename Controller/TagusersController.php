daaa3ab4cb2c46aefeafd913dc7a71868350ca59
koyakei <koyakeiaaaaa@hotmail.com>
2014-05-11 07:44:30 +0900

    id から　リクエストへ

--- Controller/TagusersController.php
+++ Controller/TagusersController.php
@@ -135,7 +135,7 @@ class TagusersController extends AppController {
 // 				array('conditions' => array('Taguser.'. $this->primaryKey => $id) //from と　toに分けるか？
 // 		));
 		$FT['Article'] = $this->Basic->tribasicfiderbyid($that,null,"Article","Article.ID",$this->params['url']['id']);
-		$FT['Tag'] = $this->Basic->tribasicfiderbyid($that,null,"Tag","Tag.ID",$id);
+		$FT['Tag'] = $this->Basic->tribasicfiderbyid($that,null,"Tag","Tag.ID",$this->params['url']['id']);
 // 		$TF['Tag'] = $this->Basic->tribasicfiderbyidTF($that,null,"Tag","Tag.ID",$id);
 // 		$TF['Article'] = $this->Basic->tribasicfiderbyidTF($that,null,"Article","Article.ID",$id);
 		//制限要素　user_id.trikey_id

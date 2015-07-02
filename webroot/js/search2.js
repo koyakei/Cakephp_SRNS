6300467ddf9b219aa55d2562db9d12d8a09b3cec
koyakei <koyakeiaaaaa@hotmail.com>
2015-07-02 14:11:55 +0900

    autocomplete 作動う不良原因

--- webroot/js/search2.js
+++ webroot/js/search2.js
@@ -1,20 +1,20 @@
-$(document).ready(
-function all_search_finder(){
-		var i = null;
-		var Search_conditions = [];
-		var sorting_tags = $(".sorting_tags .search_tag_id .tag_id").val();
-		var trikeys = [];
-		for(i=0;i<=1;i++){
-    		Search_conditions[i] = $(".search_tag_id").children("input[name*='data[or]["+ i +"]']").map(function() { return $(this).val();});
-    	}
-    	$.ajax({
-            type: "GET",
-            url: location.origin +"/cakephp/tags/GET_all_search",
-            data: {searching_tag_ids:[[Search_conditions[0][0],Search_conditions[0][1]],[Search_conditions[1][0],Search_conditions[1][1]]],trikeys : trikeys,sorting_tags : sorting_tags},
-            dataType:'html',
-            success: function(data){
-            	$(".root").html(data);
-            },
-        });
-	}
-)
+//$(document).ready(
+//function all_search_finder(){
+//		var i = null;
+//		var Search_conditions = [];
+//		var sorting_tags = $(".sorting_tags .search_tag_id .tag_id").val();
+//		var trikeys = [];
+//		for(i=0;i<=1;i++){
+//    		Search_conditions[i] = $(".search_tag_id").children("input[name*='data[or]["+ i +"]']").map(function() { return $(this).val();});
+//    	}
+//    	$.ajax({
+//            type: "GET",
+//            url: location.origin +"/cakephp/tags/GET_all_search",
+//            data: {searching_tag_ids:[[Search_conditions[0][0],Search_conditions[0][1]],[Search_conditions[1][0],Search_conditions[1][1]]],trikeys : trikeys,sorting_tags : sorting_tags},
+//            dataType:'html',
+//            success: function(data){
+//            	$(".root").html(data);
+//            },
+//        });
+//	}
+//)
